1541eb8d5c59d749c0cc4524b0748d63181faa3d
MarkPiccirilli <36469760+MarkPiccirilli@users.noreply.github.com>
2018-02-16 23:10:37 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,29 +4,65 @@
 #include "critter.hpp"
 #include "ant.hpp"
 #include "doodlebug.hpp"
+#include "inVal.hpp"
 #include <time.h>
 
-using namespace std;
+using std::cin;
+using std::cout;
+using std:: endl;
 
 int main()
 {
-    srand(time(NULL));
+    cout << "We did the extra credit!!!!!" << endl << endl;
 
-    Board b1;
+    srand(time(NULL)); //im not sure what this is doing
 
-	Critter* c1 = new Ant();
-	Critter* c2 = new Doodlebug();
-
-	Critter*** g1 = b1.getGrid();
-    g1[5][18] = c1;
-	g1[4][16] = c2;
-
+    //prompt user for Board size
+    cout << "Please enter number of rows in grid: ";
+    int rows = inVal(999, 999, 0, 0);
+    cout << "Please enter number of columns in grid: ";
+    int columns = inVal(999, 999, 0, 0);
+    //overload constructor that I dont think is created yeat
+    Board b1(rows, columns);
+    //print board
     b1.printBoard();
 
-    for(int i = 0; i < 10; i++)
+    Critter*** g1 = b1.getGrid();
+
+    //ompt user for number of ants
+    cout << "Please enter the starting number of Ants: ";
+    int numAnts = inVal(200, 200, 50, 50);
+    for(int i=0; i<numAnts; i++) {
+       Critter c1 = new Ant();
+       int r, c;
+       do {
+          r = rand % b1.getRows() + 1;
+          c = rand % b1.getColumns() + 1;
+       } while(g1[r][c] != NULL)
+       g1[r][c] = c1;
+    }
+
+    //promput user for number of doodle bugs
+    cout << "Please enter number of doodlebugs: ";
+    int numDoodle =inVal(20, 20, 2, 2);
+    for(int i=0; i<numDoodle; i++) {
+       Critter* c2 = new Doodle();
+       int r, c;
+       do {
+          r = rand % b1.getRows() + 1;
+          c = rand % b1.getColumns() + 1;
+       } while(g1[r][c] != NULL)
+       g1[r][c] = c1;
+    }
+
+    //prompt user for number steps
+    cout << "Please enter the number of steps in the simuation: ";
+    int steps = inVal(99999, 999999, 1, 1);
+
+    for(int i = 0; i < steps; i++)
     {
-		b1.checkDoodleMove(); 
-		b1.checkAntMove();
+        b1.checkDoodleMove();
+        b1.checkAntMove();
         b1.checkAntBreed();
         b1.advanceAge();
         b1.printBoard();
