23a2b326a429c5699d4c8efcc35343a3c31921dd
MarkPiccirilli <36469760+MarkPiccirilli@users.noreply.github.com>
2018-02-16 22:59:59 -0500

    Update board.hpp
    changes for the extra credit to choose board size

--- board.hpp
+++ board.hpp
@@ -8,10 +8,17 @@ class Board
     private:
         Critter*** grid;
         int critterCount;
+	int rows;
+	int columns;
 
     public:
         Board();
+	Board(int, int);
         ~Board();
+	int getRows();
+	int getColumns();
+	void setRows(int );
+	void setColumns(int );
         Board getBoard();
         Critter*** getGrid();
         void printBoard();
